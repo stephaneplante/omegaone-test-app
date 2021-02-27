e35cf560e5fbaae7619a0cf6a164835ee6459fa8
stephaneplante <stephane.plante@omegaone-soft.com>
2021-02-27 00:28:27 -0500

    Créer le projet de base

---
+++ omegaone-test-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace omegaone_test_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
