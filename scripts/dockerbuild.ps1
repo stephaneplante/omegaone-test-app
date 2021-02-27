cc533926e3e0741fa2318660d9c55e46edc67490
Stephane Plante <stephane.plante@omegaone-soft.com>
2021-02-27 13:59:46 -0500

    add scripts to publish image

---
+++ scripts/dockerbuild.ps1
@@ -0,0 +1,3 @@
+cd ./app
+docker build -t omegaone-test-app .
+cd ..
---
+++ scripts/herokupublsh.ps1
@@ -0,0 +1,4 @@
+cd ./app
+heroku container:push -a omegaone-test-app web
+heroku container:release -a omegaone-test-app web
+cd ..
